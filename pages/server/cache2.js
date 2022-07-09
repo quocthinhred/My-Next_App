4da6475d435554774520fbcb978c8c418e9f2d07
Trần Quốc Thịnh <43133530+quocthinhred@users.noreply.github.com>
2022-07-10 01:44:42 +0700

    Update cache2.js

--- pages/server/cache2.js
+++ pages/server/cache2.js
@@ -33,7 +33,7 @@ const Post = ({ post }) => {
 }
 
 export const getServerSideProps = async (context) => {
-    context.res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+    context.res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate');
     await new Promise((resolve)=>{setTimeout(resolve, 3000)});
     const post = await getSomeThing();
     return {
