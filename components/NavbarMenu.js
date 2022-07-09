88368571bb0ebb824dda31b3d2bdec4552134609
Trần Quốc Thịnh <43133530+quocthinhred@users.noreply.github.com>
2022-07-10 01:31:41 +0700

    Update NavbarMenu.js

--- components/NavbarMenu.js
+++ components/NavbarMenu.js
@@ -13,6 +13,9 @@ const NavbarMenu = () => (
                 <Link href='/recommend' passHref><Nav.Link>Trending</Nav.Link></Link>
                 <Link href='/posts' passHref><Nav.Link>Posts</Nav.Link></Link>
                 <Link href='/about' passHref><Nav.Link>About</Nav.Link></Link>
+                <Link href='/server/cache1' passHref><Nav.Link>Cache1</Nav.Link></Link>
+                <Link href='/server/cache2' passHref><Nav.Link>Cache2</Nav.Link></Link>
+                <Link href='/server/cache3' passHref><Nav.Link>Cache3</Nav.Link></Link>
             </div>
             <div>
                 <Link href='/cart' passHref><Nav.Link><FontAwesomeIcon icon={faCartShopping} /></Nav.Link></Link>
